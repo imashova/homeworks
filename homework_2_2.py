661f42a623076f271f47591e9a086ba005b7d5e3
imashova <annimashova@gmail.com>
2024-11-14 23:13:14 +0400

    Add files via upload

---
+++ homework_2_2.py
@@ -0,0 +1,14 @@
+first = input('Введите первое целое число: ')
+second = input('Введите второе целое число: ')
+third = input('Введите третье целое число: ')
+
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+
+
+
+
